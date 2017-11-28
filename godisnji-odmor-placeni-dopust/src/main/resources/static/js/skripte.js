7365b42b44707d05203ce267996e7d7a57783c1a
icekovic <icekovic@racunarstvo.hr>
2017-11-28 18:02:41 +0100

    novo

--- godisnji-odmor-placeni-dopust/src/main/resources/static/js/skripte.js
+++ godisnji-odmor-placeni-dopust/src/main/resources/static/js/skripte.js
@@ -67,7 +67,6 @@ $(document).ready(function(){
 	    		
 	    		//registracija
 	    		$("#registracija").validate({
-	    			errorClass: 'errors',
 	    			rules: {
 	    				ime: {
 	    					required: true
